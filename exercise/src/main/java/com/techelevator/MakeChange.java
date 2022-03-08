8737eaf35a659ab8ee52f2bc321d8050312feeb0
Edwin Reyes <er0688@gmail.com>
2022-03-08 18:47:36 -0500

    Completed MakeChange Project

--- exercise/src/main/java/com/techelevator/MakeChange.java
+++ exercise/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,6 +14,23 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Double amountTendered = 0.0;
+		Double amountDue = 0.0;
+		Double changeRequired = 0.0;
+
+		System.out.println("Please enter the amount of the bill: ");
+		amountDue = scanner.nextDouble();
+
+
+		System.out.println("Please enter amount tendered: ");
+		amountTendered = scanner.nextDouble();
+
+		changeRequired =  (amountTendered - amountDue);
+
+		System.out.println("Change required is: " + changeRequired);
+
 
 	}
 
