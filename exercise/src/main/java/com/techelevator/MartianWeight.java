0d1846cd69f3b2679efc90cc0acf4b7229f0ce34
Edwin Reyes <er0688@gmail.com>
2022-03-08 19:36:12 -0500

    Completed MartianWeight Project

--- exercise/src/main/java/com/techelevator/MartianWeight.java
+++ exercise/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
 In case you've ever pondered how much you weigh on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,7 +21,27 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double earthWeightOne = 0.0;
+		double earthWeightTwo = 0.0;
+		double earthWeightThree = 0.0;
 
+		System.out.println("Please enter weight: ");
+
+		earthWeightOne = Double.parseDouble(scanner.next());
+		earthWeightTwo = Double.parseDouble(scanner.next());
+		earthWeightThree = Double.parseDouble(scanner.next());
+
+		double marsWeightOne = 0.0;
+		marsWeightOne = (earthWeightOne * 0.378);
+		double marsWeightTwo = 0.0;
+		marsWeightTwo = (earthWeightTwo * 0.378);
+		double marsWeightThree = 0.0;
+		marsWeightThree = (earthWeightThree * 0.378);
+
+		System.out.println(earthWeightOne + " lbs. on Earth, is " + marsWeightOne + " lbs. on Mars.");
+		System.out.println(earthWeightTwo + " lbs. on Earth, is " + marsWeightTwo + " lbs. on Mars.");
+		System.out.println(earthWeightThree + " lbs. on Earth, is " + marsWeightThree + " lbs. on Mars.");
 	}
 
 }
